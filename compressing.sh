39ad401418b8a8ca701d6582ba11aa105405da72
ahmed-debbech <debbech.ahmed@gmail.com>
2020-02-28 18:50:12 +0100

    Add a check of file in compressing section

--- compressing.sh
+++ compressing.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-checkExit(){
+checkExist(){
         fold="/backup/${USER}"
-        if [[ -d ${fold} ]]; then
-                echo -e "No archive found to compress!\n"
-                exit 0
+        if [[ -d ${fold}.tar ]]; then
+                echo "archive found"
+                return 0
         else
-                exit 1
+		echo "No archive found to compress"
+                return 1
         fi
 }
-
+menu(){
+	x = -1;
+	until [[ $x -eq 1 || $x -eq 2 ]]
+	do
+		echo "1 : compress in gz format"
+		echo "2 : compress in xy format"
+		read x;
+	done
+	case $x in
+		1) return 1;
+			;;
+		2) return 2;
+			;;
+	esac
+}
 compress(){
+	ret=${checkExist}
 	echo "Compressing... Please Wait"
-	fold="/backup"
-	tar -cvzf ${fold}.tar.gz ${fold}/${USER}
-	echo "Done compressing."
+	if [[ $ret -eq 0 ]]; then
+		frmt=${menu}
+		fold="/backup/${USER}"
+		if [[ ${frmt} -eq 1 ]]; then
+		sudo tar -czf ${fold}.tar.gz ${fold}.tar
+		else
+		sudo tar -czf ${fold}.tar.xy ${fold}.tar
+		fi
+		sudo rm ${fold}.tar
+		echo "Done compressing."
+	fi
 }
 
