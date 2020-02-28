1592fa045f9a3b7a615ba22f0ee34a2c7d344844
ahmed-debbech <debbech.ahmed@gmail.com>
2020-02-28 22:58:01 +0100

    Fix compression mechanism

--- compressing.sh
+++ compressing.sh
@@ -2,36 +2,41 @@
 
 checkExist(){
         fold="/backup/${USER}"
-        if [[ -d ${fold}.tar ]]; then
+        if [[ -f ${fold}.tar ]]; then
                 echo "archive found"
-                return 0
+                ret=0
         else
 		echo "No archive found to compress"
-                return 1
+                ret=1
         fi
 }
 menu(){
-	x = -1;
 	until [[ $x -eq 1 || $x -eq 2 ]]
 	do
 		echo "1 : compress in gz format"
 		echo "2 : compress in xy format"
 		read x;
 	done
-	case $x in
-		1) return 1;
+	case ${x} in
+		1) x = 1;
 			;;
-		2) return 2;
+		2) x = 2;
 			;;
 	esac
 }
 compress(){
-	ret=${checkExist}
+	ret=$(checkExist) 
 	echo "Compressing... Please Wait"
-	if [[ $ret -eq 0 ]]; then
-		frmt=${menu}
+	echo $ret
+	if [[ ${ret} == "archive found" ]]; then
+		until [[ $x -eq 1 || $x -eq 2 ]]
+		do
+			echo "1: compress in gz format"
+			echo "2: compress in xy format"
+			read x
+		done
 		fold="/backup/${USER}"
-		if [[ ${frmt} -eq 1 ]]; then
+		if [[ ${x} -eq 1 ]]; then
 		sudo tar -czf ${fold}.tar.gz ${fold}.tar
 		else
 		sudo tar -czf ${fold}.tar.xy ${fold}.tar
