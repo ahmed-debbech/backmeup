c4409e1027e363ee8ab0b8261afa72edde394c93
ahmed-debbech <debbech.ahmed@gmail.com>
2020-03-01 17:30:44 +0100

    Add a meaningful Graphics with YAD

--- graphics.sh
+++ graphics.sh
@@ -7,9 +7,20 @@ source compressing.sh
 source restore.sh
 
 graph(){
-    re=$(zenity --forms --title="Back Me Up" \
-    --text="Enter options sperated by space." \
-    --add-entry="for more info type -h" \
-    --ok-label="Run")
-    ./bkup.sh $re
+    action=$(yad --width 300 --title "Back Me Up" \
+    --text "Choose an option" \
+    --button="Save":1 --buttons-layout=center \
+    --button="Archive":2 \
+    --button="Compress":3 \
+    --button="Restore":4)
+    case $? in
+        1) ./bkup.sh -s
+            ;;
+        2) ./bkup.sh -ar
+            ;;
+        3) ./bkup.sh -c
+            ;;
+        4) ./bkup.sh -r
+            ;;
+    esac
 }
